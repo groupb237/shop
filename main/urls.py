14ff1bf1b00ac6a7a646a782626c29dc051159bb
Omadbek-web <omadbek.web@gmail.com>
2025-04-10 14:10:31 +0500

    fox

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 import main.views as views
 
 urlpatterns = [
-    path("", views.home)
+    path("", views.home),
+    path("category/<int:category_id>", views.category_list)
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+import users.forms as forms
+
+
 # Create your views here.
+
+def register_user(request):
+    form = forms.RegisterUser()
+    return render(request, "auth/form.html",
+                  context={"form": form})

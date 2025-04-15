c899eec5f4b222c32ab6a707d5a9ed664bb00c90
Omadbek-web <omadbek.web@gmail.com>
2025-04-15 15:07:24 +0500

    fux

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ import main.views as views
 
 urlpatterns = [
     path("", views.home),
-    path("category/<int:category_id>", views.category_list)
+    path("category/<int:category_id>", views.category_list),
+    path("create/wishlist/<int:product_id>", views.create_wishlist),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import main.models as models
+
+
 # Create your views here.
 
 def home(request):
     return render(request, "index.html")
 
+
 def category_list(request, category_id):
     products = models.Product.objects.filter(category_id=category_id)
     return render(request, "shop-categories.html", {"products": products})
+
+
+def create_wishlist(request, product_id):
+    user = request.user
+    product = models.Product.objects.get(pk=product_id)
+    models.Wishlist.objects.create(
+        user=user,
+        product=product
+    )
+    return redirect("/")
